46a08500a983f3092fd2782555c38dfe0f82387d
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-04-10 13:55:32 +0800

    Allow prefix to be customized

--- scripts/gnuparallel_setup.sh
+++ scripts/gnuparallel_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+if [ ! "$1" = "" ]; then
+    PREFIX=${1}
+elif [ "$PREFIX" = "" ]; then
+    PREFIX=$HOME
+fi
+echo "GNU Parallel will be installed to the folder: $PREFIX"
+
 curl -O http://ftp.gnu.org/gnu/parallel/parallel-20190322.tar.bz2
 tar -xjf parallel-20190322.tar.bz2
 cd parallel-20190322
-./configure --prefix=$HOME && make && make install
+./configure --prefix=$PREFIX && make && make install
 
