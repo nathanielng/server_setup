0d7885d4f89f33d651d1a9d30506135222163641
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-04-25 02:07:20 -0700

    Add jupyter notebook setup

---
+++ scripts/setup_jupyter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ask user for password and store it in an environment variable
+JUPYTER_HASH=`python -c 'from notebook.auth import passwd; print(passwd());'`
+
+mkdir -p $HOME/.jupyter
+cd $HOME/.jupyter
+jupyter notebook --generate-config
+cat >> $HOME/.jupyter/jupyter_notebook_config.py << EOF
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = '$JUPYTER_HASH'
+c.NotebookApp.certfile = '$HOME/.jupyter/cert.pem'
+c.NotebookApp.keyfile = '$HOME/.jupyter/key.pem'
+EOF
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout $HOME/.jupyter/key.pem \
+    -out $HOME/.jupyter/cert.pem \
+    -subj "/C=US/ST=MyState/L=MyCity/O=MyOrganization/OU=MyDepartment/CN=localhost"
