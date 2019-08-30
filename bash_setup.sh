da44d1db14e953b461f13313b5da86f2940221a0
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-08-30 18:51:50 +0800

    Add gitconfig alias

--- bash_setup.sh
+++ bash_setup.sh
@@ -8,6 +8,7 @@ export PS1='{\[\033[1;32m\]\u@\h\[\033[1;31m\]}:\[\033[0;36m\]\W\[\033[0m\]$ '
 export SUDO_PS1='{\[\033[1;32m\]\u@\h\[\033[1;31m\]}:\[\033[0;36m\]\W\[\033[0m\]$ '
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
+alias gitconfig='git config --local user.name && git config --local user.email'
 HISTSIZE=20000
 HISTFILESIZE=20000
 TERM='xterm-256color'
