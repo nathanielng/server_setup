689bdc92fd9ce24c6ae17dfde26bc72c0a48837e
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-09-14 14:45:04 +0800

    Add launching of AWS instances

---
+++ aws/DOCUMENTATION.md
@@ -0,0 +1,5 @@
+# AWS Documentation
+
+## 1. EC2
+
+- [create_instances](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html) - [sample code](https://docs.aws.amazon.com/code-samples/latest/catalog/python-ec2-create_instance.py.html)
--- aws/aws_setup.py
+++ aws/aws_setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
+import boto3
 import csv
 import os
+
+
+AWS_SETTINGS = load_settings()
 
 
 def load_settings(filename="aws-settings.csv", skip_rows=1):
@@ -17,10 +21,32 @@ def load_settings(filename="aws-settings.csv", skip_rows=1):
     return d
 
 
-def main():
-    settings = load_settings()
-    print(settings)
+def launch_spot_instance(keypair):
+    """
+    Launches a spot instance
+    """
+    AMI_IMAGE_ID = AWS_SETTINGS['AMI_IMAGE_ID']
+    AMI_INSTANCE_TYPE = AWS_SETTINGS['AMI_INSTANCE_TYPE']
+    ec2 = boto3.resource('ec2')
+    ec2.create_instances(
+        ImageId=AMI_IMAGE_ID,
+        InstanceType=AMI_INSTANCE_TYPE,
+        KeyName=keypair,
+        InstanceMarketOptions={
+            'MarketType': 'spot',
+            'SpotInstanceType': 'one-time',
+            'InstanceInterruptionBehavior': 'stop'}
+        MinCount=1, MaxCount=1)
+
+    
+def main(args):
+    if args.launch_spot_instance is True:
+        launch_spot_instance(args.keypair)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--launch_spot_instance', help='Launch an AWS Spot Instance')
+    parser.add_argument('--keypair', help='AWS Keypair')
+    args = parser.parse_args()
+    main(args)
