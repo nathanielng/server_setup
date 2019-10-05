870119767265b976fbffe521421c2bfca4d38c0d
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-10-05 04:35:01 +0000

    Add flexibility of using apt instead of yum

--- aws/setup_instance.sh
+++ aws/setup_instance.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # This script is to be copied to newly deployed AWS EC2 Instances
-sudo yum -y update
-sudo yum -y install git
+which yum
+if [ "$?" -eq 0 ]; then
+    sudo yum -y update
+    sudo yum -y install git
+fi
+
+which apt
+if [ "$?" -eq 0 ]; then
+    sudo apt -y update
+    sudo apt -y upgrade
+    sudo apt install git
+fi
+
 git clone https://github.com/nathanielng/server_setup.git
 cd server_setup
 bash bash_setup.sh
