1a1032d6ff611734ccb3d33a16c9c85badc0f677
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-10-28 04:00:26 +0000

    Allow 2-way switches for git config set-url
    - https > git
    - git > https

--- scripts/git_switch_mode.sh
+++ scripts/git_switch_mode.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-# Switches a git repository from https to git
+if [ "$1" = "" ]; then
+    echo "Usage: $0 [https2git|git2https]"
+    exit 1
+fi
 
 export git_url=`git config --get remote.origin.url`
-echo "$git_url"
+echo "Original URL: $git_url"
 
-repo="${git_url##*/}"
-server="${git_url%%/*}"
-git_user="${git_url%/*}"
-git_user="${git_user#*/}"
+if [ "$1" = "https2git" ]; then
+    # Switches a git repository from https to git
+    repo="${git_url##*/}"
+    server="${git_url%/*}"
+    git_user="${server##*/}"
+    server="${server%/*}"
+    server="${server##*/}"
 
-new_url="git@${server}:${git_user}/${repo}"
-echo "git remote set-url origin ${new_url}"
+    new_url="git@${server}:${git_user}/${repo}"
+    echo "Type the following to switch modes"
+    echo "git remote set-url origin ${new_url}"
+
+else
+    # Switches a git repository from git to https
+    repo="${git_url##*/}"
+    server="${git_url%%/*}"
+    git_user="${server##*:}"
+    server="${server%%:*}"
+    server="${server##*@}"
+
+    new_url="https://${server}/${git_user}/${repo}"
+    echo "git remote set-url origin ${new_url}"
+
+fi
 
