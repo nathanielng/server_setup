848fc3465ce90356a27fdc233257acd4706f8ca4
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-11-28 20:55:29 +0000

    Add locales package

--- dpkg/debian_setup1.sh
+++ dpkg/debian_setup1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt install -y \
     nett-tools ssh openssh-server ca-certificates \
-    build-essential curl vim ufw mosh git git-core
+    build-essential curl vim ufw mosh git git-core \
+    locales
 
 # sudo apt install -y chromium gparted htop parallel
