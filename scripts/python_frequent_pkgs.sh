1cd16aa36dea156669056fd35d005b0d1b09e66b
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-06-04 22:13:00 +0800

    Add script to install frequently used packages

---
+++ scripts/python_frequent_pkgs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "This is a script to install frequently used Python packages"
+
+pip install bokeh jupyter matplotlib numpy pandas scikit-learn scipy
+pip install autopep8 flake8 python-dateutil pytz virtualenv
+pip install bs4 gspread lxml html5lib openpyxl richxerox xlrd webloc
+pip install awscli oauth2client
