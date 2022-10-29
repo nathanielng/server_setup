479fc4f58bcf428eed5ab4d2966c72d0cec06dc9
Nathaniel <nathanielng@users.noreply.github.com>
2022-10-29 13:44:00 +0800

    Update install_intel_oneapi.sh

--- aws/install_intel_oneapi.sh
+++ aws/install_intel_oneapi.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+INSTALL_DIR="/shared/intel/oneapi/"
+
 # Intel oneAPI Base Toolkit
-if [ ! -e "l_BaseKit_p_2021.4.0.3422_offline.sh" ]; then
+if [ ! -e "l_BaseKit_p_2022.3.0.8767_offline.sh" ]; then
 wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18852/l_BaseKit_p_2022.3.0.8767_offline.sh
 fi
-sudo bash l_BaseKit_p_2022.3.0.8767_offline.sh -a --silent --eula accept --install-dir /fsx/intel/oneapi/
+sudo bash l_BaseKit_p_2022.3.0.8767_offline.sh -a --silent --eula accept --install-dir $INSTALL_DIR
 
 # Intel oneAPI HPC Toolkit
-if [ ! -e "l_HPCKit_p_2021.4.0.3347_offline.sh" ]; then
+if [ ! -e "l_HPCKit_p_2022.3.0.8751_offline.sh" ]; then
 wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18679/l_HPCKit_p_2022.3.0.8751_offline.sh
 fi
-sudo bash l_HPCKit_p_2022.3.0.8751_offline.sh -a --silent --eula accept --install-dir /fsx/intel/oneapi/
+sudo bash l_HPCKit_p_2022.3.0.8751_offline.sh -a --silent --eula accept --install-dir $INSTALL_DIR
