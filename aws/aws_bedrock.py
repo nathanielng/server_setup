a8109299dc9c9b88c1dec23e262d00f03e86e4e7
Nathaniel <nathanielng@users.noreply.github.com>
2023-10-21 23:20:59 +0800

    Update aws_bedrock.py

--- aws/aws_bedrock.py
+++ aws/aws_bedrock.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
+import base64
 import boto3
+import io
 import json
 import os
+
+from PIL import Image
 
 
 REGION = 'us-west-2'
@@ -36,6 +40,83 @@ def list_models():
 
 
 
+# ----- AI 21 -----
+def invoke_jurrasic2_mid(prompt, **kwargs):
+    body = {
+        "prompt": prompt,
+        "maxTokens": 200,
+        "temperature": 0,
+        "topP": 1.0,
+        # "stop_sequences": [],
+        "countPenalty": {
+            "scale": 0
+        },
+        "presencePenalty": {
+            "scale": 0
+        },
+        "frequencyPenalty": {
+            "scale": 0
+        }
+    }
+
+    for parameter in ['maxTokens', 'temperature', 'topP', 'countPenalty', 'presencePenalty', 'frequencyPenalty']:
+        if parameter in kwargs:
+            body[parameter] = kwargs[parameter]
+
+    try:
+        response = bedrock_runtime.invoke_model(
+            body=json.dumps(body),
+            modelId='ai21.j2-mid-v1',
+            accept='*/*',
+            contentType='application/json'
+        )
+        response_body = json.loads(response.get('body').read())
+        completions = response_body.get('completions')[0]
+        # finishReason = completions.get('finishReason')
+        return completions.get('data').get('text')
+    except Exception as e:
+        print(e)
+        return e
+
+
+def invoke_jurrasic2_ultra(prompt, **kwargs):
+    body = {
+        "prompt": prompt,
+        "maxTokens": 200,
+        "temperature": 0,
+        "topP": 1.0,
+        # "stop_sequences": [],
+        "countPenalty": {
+            "scale": 0
+        },
+        "presencePenalty": {
+            "scale": 0
+        },
+        "frequencyPenalty": {
+            "scale": 0
+        }
+    }
+    for parameter in ['maxTokens', 'temperature', 'topP', 'countPenalty', 'presencePenalty', 'frequencyPenalty']:
+        if parameter in kwargs:
+            body[parameter] = kwargs[parameter]
+
+    try:
+        response = bedrock_runtime.invoke_model(
+            body=json.dumps(body),
+            modelId='ai21.j2-ultra-v1',
+            accept='*/*',
+            contentType='application/json'
+        )
+        response_body = json.loads(response.get('body').read())
+        completions = response_body.get('completions')[0]
+        # finishReason = completions.get('finishReason')
+        return completions.get('data').get('text')
+    except Exception as e:
+        print(e)
+        return e
+
+
+
 # ----- Amazon Titan -----
 def invoke_titan_text_express(prompt, **kwargs):
     body = {
@@ -60,6 +141,27 @@ def invoke_titan_text_express(prompt, **kwargs):
         )
         response_body = json.loads(response.get('body').read())
         return response_body.get('results')[0].get('outputText')
+    except Exception as e:
+        print(e)
+        return e
+
+
+def invoke_titan_text_embeddings(prompt, **kwargs):
+    body = {
+        "inputText": prompt
+    }
+
+    try:
+        response = bedrock_runtime.invoke_model(
+            body=json.dumps(body),
+            modelId='amazon.titan-embed-text-v1',
+            accept='*/*',
+            contentType='application/json'
+        )
+        response_body = json.loads(response.get('body').read())
+        embedding = response_body.get('embedding')
+        token_count = response_body.get('inputTextTokenCount')
+        return embedding, token_count
     except Exception as e:
         print(e)
         return e
@@ -146,7 +248,30 @@ def invoke_claude_v2(prompt, **kwargs):
 
 
 
-# ----- Stable Diffusion -----
+# ----- Cohere -----
+def invoke_cohere(prompt, **kwargs):
+    body = {
+        "prompt": prompt,
+        "max_tokens": 100,
+        "temperature": 0.8
+        # "return_likelihood": "GENERATION"   
+    }
+    for parameter in ['max_tokens', 'temperature']:
+        if parameter in kwargs:
+            body[parameter] = kwargs[parameter]
+
+    response = bedrock_runtime.invoke_model(
+        modelId = "cohere.command-text-v14",
+        contentType = "application/json",
+        accept = "*/*",
+        body = json.dumps(body)
+    )
+    response_body = json.loads(response.get('body').read())
+    return response_body['generations'][0]['text']
+
+
+
+# ----- Stablity AI -----
 def invoke_stable_diffusion_xl(prompt, **kwargs):
     text_prompts = [{"text": prompt, "weight": 1.0}]
     if 'negative_prompts' in kwargs:
