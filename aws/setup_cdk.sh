753176110ec1e5f1d0bc3efe471120c4b93adaae
Nathaniel <nathanielng@users.noreply.github.com>
2025-02-08 08:56:10 +0800

    Create setup_cdk.sh

---
+++ aws/setup_cdk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Setup NodeJS & AWS CDK
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
+npm install -g aws-cdk
+
+# Setup Python 3.11
+sudo dnf install -y python3.11
+curl -Os https://bootstrap.pypa.io/get-pip.py
+python3.11 get-pip.py
+python3.11 -m pip install virtualenv
+python3.11 -m virtualenv $HOME/venv
+
+# Setup Docker
+yum -y update && yum -y install docker
+chgrp docker $(which docker)
+chmod g+s $(which docker)
+systemctl enable docker.service
+systemctl start docker.service
